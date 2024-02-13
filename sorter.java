34127a949190e3f2979bca66cd35a57cef6b9700
MohamedAlmuhanna <s202152750@kfupm.edu.sa>
2024-02-13 16:31:51 +0300

    merge

--- sorter.java
+++ sorter.java
@@ -1,8 +1,9 @@
 public class Sorter{
     //sorter
     public void inertionsort(String o ){}
-    public void selectionsort(String x){
+    public void selectionsort(String x){}
+    public void sort(String x){
+        inertionsort(op)
 
-        
     }
 }
